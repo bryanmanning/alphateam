b584b1fd8d5571c226548cbc496f25c4f719254b
Mike Cassano <mcassano@gmail.com>
2014-01-19 14:32:10 -0600

    mcassano: first version with basic index.php

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<?php
+$file = "players"
+$num_players = file_get_contents($file);
+$myNum = $num_players;
+file_put_contents($file, $num_players++);
+?>
+
+<html>
+<head>
+<title>alphateam</title>
+</head>
+<body>
+<!-- <?php echo rand(1000000000, 9999999999); ?> -->
+  <button id="myBtn" value="<?php =$myNum; ?>"/>
+</body>
+</html>
